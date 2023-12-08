42b5032c3220d9bad493ca39ad6c20f745ebeb9f
catandA <75292299+catandA@users.noreply.github.com>
2023-12-08 19:28:05 +0800

    v0.2.3 补全错误处理-无skb档案

--- src/main/java/com/catand/skyblockhelper/ErrorProcessor.java
+++ src/main/java/com/catand/skyblockhelper/ErrorProcessor.java
@@ -25,7 +25,9 @@ public class ErrorProcessor {
 				sendMsg = MsgUtils.builder().text("你的数据目前被Hypixel打乱了\n暂时查不出来\n(￣ε(#￣)☆╰╮(￣▽￣///)\n经SkyCrypt确认，此问题由Hypixel更新skyblock数据格式引起\n在数据迁移未完成期间上号会导致此问题\n得等Hypixel修它的API");
 			} else if (exception.getMessage().contains("404 Not Found")) {
 				sendMsg = MsgUtils.builder().text("参数格式打错了\n正确格式：/networth <玩家名>");
-			}else {
+			}else if (exception.getMessage().contains("Player has no SkyBlock profiles")) {
+				sendMsg = MsgUtils.builder().text("此玩家没有skyblock档案");
+			}else{
 				sendMsg = MsgUtils.builder().text("未知错误,爆!");
 			}
 		}else if (exception instanceof ResourceAccessException) {
