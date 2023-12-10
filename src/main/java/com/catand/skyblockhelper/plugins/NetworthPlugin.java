35d100ae65c9e99794d0a2def354e4b23bc8dd39
catandA <75292299+catandA@users.noreply.github.com>
2023-12-10 23:03:37 +0800

    使用Java2D 完成身价的图形化返回 ImageUtil

--- src/main/java/com/catand/skyblockhelper/plugins/NetworthPlugin.java
+++ src/main/java/com/catand/skyblockhelper/plugins/NetworthPlugin.java
@@ -3,6 +3,7 @@ package com.catand.skyblockhelper.plugins;
 import com.alibaba.fastjson2.JSONObject;
 import com.catand.skyblockhelper.ErrorProcessor;
 import com.catand.skyblockhelper.Player;
+import com.catand.skyblockhelper.utils.ImageUtil;
 import com.catand.skyblockhelper.utils.NumberFormatUtil;
 import com.catand.skyblockhelper.utils.ProfileUtil;
 import com.mikuac.shiro.common.utils.MsgUtils;
@@ -10,6 +11,11 @@ import com.mikuac.shiro.core.Bot;
 import com.mikuac.shiro.core.BotPlugin;
 import com.mikuac.shiro.dto.event.message.GroupMessageEvent;
 import org.springframework.stereotype.Component;
+
+import java.awt.*;
+import java.awt.geom.RoundRectangle2D;
+import java.awt.image.BufferedImage;
+import java.text.DecimalFormat;
 
 @Component
 public class NetworthPlugin extends BotPlugin {
@@ -38,37 +44,128 @@ public class NetworthPlugin extends BotPlugin {
 			Player player = new Player(args[1]);
 			JSONObject networthData = ProfileUtil.getNetworthData(player.getMainProfile());
 			JSONObject networthTypesData = networthData.getJSONObject("types");
+			String profileName = ProfileUtil.getProfileName(player.getMainProfile());
 
-			sendMsg = MsgUtils.builder().text(player.name + "在" + ProfileUtil.getProfileName(player.getMainProfile()) + "上的身价:\n" +
-					"总计:" + NumberFormatUtil.format(networthData.getDoubleValue("networth")) + "\t");
-			String[] keys = networthData.getJSONObject("types").keySet().toArray(new String[0]);
+			MsgUtils sendMsg = MsgUtils.builder();
 
-			for (int i = 0; i < keys.length; i++) {
-				String key = keys[i];
-				String value = NumberFormatUtil.format((long) networthTypesData.getJSONObject(key).getDoubleValue("total"));
-				switch (key) {
-					case "armor" -> key = "装备";
-					case "equipment" -> key = "饰品";
-					case "wardrobe" -> key = "衣橱";
-					case "inventory" -> key = "背包";
-					case "enderchest" -> key = "末影箱";
-					case "accessories" -> key = "护符";
-					case "personal_vault" -> key = "保险箱";
-					case "storage" -> key = "存储";
-					case "fishing_bag" -> key = "钓鱼袋";
-					case "potion_bag" -> key = "药水袋";
-					case "candy_inventory" -> key = "糖果袋";
-					case "sacks" -> key = "袋子";
-					case "essence" -> key = "精粹";
-					case "pets" -> key = "宠物";
-				}
-				sendMsg.text(key + ":\t" + value);
-				if ((i + 1) % 3 == 2) {
-					sendMsg.text("\n");
-				} else {
-					sendMsg.text("\t");
+			// 获取原始 BufferedImage 对象的宽度和高度
+			int width = 960;
+			int height = 500;
+
+			Color grey = new Color(40, 40, 40);
+			Color white = new Color(212, 212, 212);
+			Color deeper = new Color(30, 30, 30, 200);
+
+			// 创建随机背景底图，添加上边栏
+			BufferedImage image = ImageUtil.getBackground(width, height);
+			image = ImageUtil.addHeader(image, 70, grey);
+			height = height + 70;
+
+			// 获取 Graphics2D 对象，创建字体实例并设置像素大小
+			Graphics2D g2d = image.createGraphics();
+			Font font = new Font("fonts/NotoSansSC-Bold.ttf", Font.BOLD, ImageUtil.getFontPixelSize(35));
+			g2d.setFont(font);
+
+			// 创建四个圆角矩形
+			g2d.setColor(white);
+			RoundRectangle2D roundedRectangle = new RoundRectangle2D.Float(15, 15, 210, 40, 20, 20);
+			g2d.fill(roundedRectangle);
+			roundedRectangle.setRoundRect(255, 15, 210, 40, 20, 20);
+			g2d.fill(roundedRectangle);
+			roundedRectangle.setRoundRect(495, 15, 210, 40, 20, 20);
+			g2d.fill(roundedRectangle);
+			roundedRectangle.setRoundRect(735, 15, 210, 40, 20, 20);
+			g2d.fill(roundedRectangle);
+
+			// 计算字符串的起始位置，使其居中到圆角矩形
+			g2d.setColor(Color.gray);
+			float startY = 35 - (float) g2d.getFontMetrics().getHeight() / 2 + g2d.getFontMetrics().getAscent();
+			float startX = 120 - (float) g2d.getFontMetrics().stringWidth(player.name) / 2;
+			g2d.drawString(player.name, startX, startY);
+			startX = 360 - (float) g2d.getFontMetrics().stringWidth(profileName) / 2;
+			g2d.drawString(profileName, startX, startY);
+			startX = 600 - (float) g2d.getFontMetrics().stringWidth("还没做你别急") / 2;
+			g2d.drawString("还没做你别急", startX, startY);
+			startX = 840 - (float) g2d.getFontMetrics().stringWidth("还没做你别急") / 2;
+			g2d.drawString("还没做你别急", startX, startY);
+
+			String[] keys = networthData.getJSONObject("types").keySet().toArray(new String[0]); // 这里应该是你的数据
+			int dataPerRow = 3; // 每行的数据数量
+			int rows = (int) Math.ceil((double) keys.length / dataPerRow); // 计算行数
+
+			startX = 20;
+			startY = 90;
+			float rowHeight = (height - startY) / (rows + 1); // 计算每行的高度
+			float columnWidth = (float) (width - 40) / dataPerRow; // 计算每列的宽度
+			String data = null;
+
+
+			// 绘制圆角矩形
+			roundedRectangle.setRoundRect(startX + 20, startY, 880, rowHeight - 20, 20, 20);
+			g2d.setColor(deeper);
+			g2d.fill(roundedRectangle);
+
+			// 在圆角矩形上绘制数据
+			DecimalFormat decimalFormat = new DecimalFormat();
+			decimalFormat.setGroupingUsed(true);
+			decimalFormat.setGroupingSize(3);
+			String networth = "总计身价:" + decimalFormat.format((long) networthData.getDoubleValue("networth"));
+			g2d.setColor(white);
+			g2d.drawString(networth,
+					(startX + 20) + 440 - ((float) g2d.getFontMetrics().stringWidth(networth) / 2),
+					startY + rowHeight / 2 - 10 - (float) g2d.getFontMetrics().getHeight() / 2 + g2d.getFontMetrics().getAscent());
+
+			// 更新 startY 以便在新的一行开始绘制循环数据
+			startY += rowHeight;
+
+
+			// 遍历每行
+			for (int i = 0; i < rows; i++) {
+				// 遍历每列
+				for (int j = 0; j < dataPerRow; j++) {
+					// 计算当前数据的索引
+					int dataIndex = i * dataPerRow + j;
+					// 如果当前索引小于数据的总数，那么我们就绘制这个数据
+					if (dataIndex < keys.length) {
+						// 计算数据的位置
+						float dataX = j * columnWidth + startX;
+						float dataY = i * rowHeight + startY;
+
+						// 绘制圆角矩形
+						roundedRectangle.setRoundRect(dataX + 20, dataY, columnWidth - 40, rowHeight - 20, 20, 20);
+						g2d.setColor(deeper);
+						g2d.fill(roundedRectangle);
+
+						// 在圆角矩形上绘制数据
+						data = NumberFormatUtil.format((long) networthTypesData.getJSONObject(keys[dataIndex]).getDoubleValue("total"));
+						switch (keys[dataIndex]) {
+							case "armor" -> keys[dataIndex] = "装备";
+							case "equipment" -> keys[dataIndex] = "饰品";
+							case "wardrobe" -> keys[dataIndex] = "衣橱";
+							case "inventory" -> keys[dataIndex] = "背包";
+							case "enderchest" -> keys[dataIndex] = "末影箱";
+							case "accessories" -> keys[dataIndex] = "护符";
+							case "personal_vault" -> keys[dataIndex] = "保险箱";
+							case "storage" -> keys[dataIndex] = "存储";
+							case "fishing_bag" -> keys[dataIndex] = "钓鱼袋";
+							case "potion_bag" -> keys[dataIndex] = "药水袋";
+							case "candy_inventory" -> keys[dataIndex] = "糖果袋";
+							case "sacks" -> keys[dataIndex] = "袋子";
+							case "essence" -> keys[dataIndex] = "精粹";
+							case "pets" -> keys[dataIndex] = "宠物";
+						}
+						data = keys[dataIndex] + ":" + data;
+						dataX = dataX + 20 + columnWidth / 2 - 20 - (float) g2d.getFontMetrics().stringWidth(data) / 2;
+						dataY = dataY + rowHeight / 2 - 10 - (float) g2d.getFontMetrics().getHeight() / 2 + g2d.getFontMetrics().getAscent();
+						g2d.setColor(white);
+						g2d.drawString(data, dataX, dataY);
+					}
 				}
 			}
+
+
+			g2d.dispose();
+			sendMsg.img(ImageUtil.ImageToBase64(image));
 			bot.sendGroupMsg(event.getGroupId(), sendMsg.build(), false);
 		} catch (Exception e) {
 			new ErrorProcessor(e, bot, event);
---
+++ src/main/java/com/catand/skyblockhelper/utils/ImageUtil.java
@@ -0,0 +1,89 @@
+package com.catand.skyblockhelper.utils;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.geom.Rectangle2D;
+import java.awt.geom.RoundRectangle2D;
+import java.awt.image.BufferedImage;
+import java.awt.image.ConvolveOp;
+import java.awt.image.Kernel;
+import java.io.*;
+import java.util.Base64;
+import java.util.Random;
+
+public class ImageUtil {
+	public static BufferedImage getBackground(int width, int height) throws IOException {
+		// 创建一个指定长宽的图片
+		BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+
+		// 获取 Graphics2D 对象
+		Graphics2D g2d = image.createGraphics();
+
+		// 创建一个File对象，获取该目录下的所有.png文件
+		File dir = new File("src/main/resources/background/");
+		File[] files = dir.listFiles((dir1, name) -> name.endsWith(".png"));
+
+		// 随机选择一个文件
+		File file = files[new Random().nextInt(files.length)];
+
+		// 加载背景图像
+		InputStream bgImageStream = new FileInputStream(file);
+		BufferedImage bgImage = ImageIO.read(bgImageStream);
+
+		// 计算背景图像的缩放比例和位置
+		float scale = Math.max((float) width / bgImage.getWidth(), (float) height / bgImage.getHeight());
+		int scaledWidth = (int) (bgImage.getWidth() * scale);
+		int scaledHeight = (int) (bgImage.getHeight() * scale);
+		int left = (width - scaledWidth) / 2;
+		int top = (height - scaledHeight) / 2;
+
+		// 创建源矩形和目标矩形
+		Rectangle2D srcRect = new Rectangle2D.Float(0, 0, bgImage.getWidth(), bgImage.getHeight());
+		Rectangle2D dstRect = new Rectangle2D.Float(left, top, scaledWidth, scaledHeight);
+
+		// 绘制背景图像
+		g2d.drawImage(bgImage, (int) dstRect.getX(), (int) dstRect.getY(), (int) dstRect.getWidth(), (int) dstRect.getHeight(), null);
+
+		g2d.dispose();
+
+		return image;
+	}
+
+	public static String ImageToBase64(BufferedImage image) throws IOException {
+		// 创建一个用于输出的ByteArrayOutputStream
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+		// 将BufferedImage编码成JPEG格式，并写入输出流
+		ImageIO.write(image, "png", outputStream);
+
+		// 使用Base64编码字节流
+		return "base64://" + Base64.getEncoder().encodeToString(outputStream.toByteArray());
+	}
+
+	public static BufferedImage addHeader(BufferedImage image, int headerHeight, Color color) {
+		// 获取原始 BufferedImage 对象的宽度和高度
+		int width = image.getWidth();
+		int height = image.getHeight();
+
+		// 创建一个新的 BufferedImage 对象，其高度是原始 BufferedImage 对象的高度加上边的高度
+		BufferedImage newImage = new BufferedImage(width, height + headerHeight, BufferedImage.TYPE_INT_ARGB);
+
+		// 获取 Graphics2D 对象
+		Graphics2D g2d = newImage.createGraphics();
+
+		// 绘制边
+		g2d.setColor(color);
+		g2d.fillRect(0, 0, width, headerHeight);
+
+		// 绘制原始 BufferedImage 对象的图像
+		g2d.drawImage(image, 0, headerHeight, null);
+
+		g2d.dispose();
+
+		return newImage;
+	}
+
+	public static int getFontPixelSize(int fontSize) {
+		return (int) (72.0 * fontSize / 96);
+	}
+}
